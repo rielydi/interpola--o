98f5b144f93901623ffb60d91a95ed856956d0f9
AUTOGLASS\rielydi.dias <rielydi.dias@autoglass.com.br>
2023-08-08 13:55:54 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diferentes formas de interpolação de string.
+            double price = 10.2;
+            //string texto = $"O preço do produto é {price}";
+            string texto = string.Format("")
+            Console.WriteLine(texto);
+        }
+    }
+}
